2bdddc504cdbca567d4fc6befa8d9e48f642e8fb
LeeGeonu <rjsdndl909@gmail.com>
2024-08-08 23:20:48 +0900

    test: JWT token generation and add tests

--- src/main/java/com/project/cafelogproject/config/SecurityConfig.java
+++ src/main/java/com/project/cafelogproject/config/SecurityConfig.java
@@ -1,19 +1,25 @@
 package com.project.cafelogproject.config;
 
+import com.project.cafelogproject.service.UserDetailService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+  private final UserDetailService userService;
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http
@@ -31,9 +37,18 @@ public class SecurityConfig {
 
     return http.build();
   }
+@Bean
+public AuthenticationManager authenticationManager(HttpSecurity http,
+    BCryptPasswordEncoder bCryptPasswordEncoder, UserDetailService userDetailService)
+    throws Exception {
+  DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+  authProvider.setPasswordEncoder(bCryptPasswordEncoder);
+  authProvider.setUserDetailsService(userService);
+  return new ProviderManager(authProvider);
 
+}
   @Bean
-  public PasswordEncoder passwordEncoder() {
+  public BCryptPasswordEncoder bCryptPasswordEncoder() {
     return new BCryptPasswordEncoder();
   }
 }
