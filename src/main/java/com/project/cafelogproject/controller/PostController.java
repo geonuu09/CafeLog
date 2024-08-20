42e3a8a3521b208b13c1e59c505c0f73c3d2e148
LeeGeonu <rjsdndl909@gmail.com>
2024-08-20 17:45:27 +0900

    refactor: PostController 주석 추가

--- src/main/java/com/project/cafelogproject/controller/PostController.java
+++ src/main/java/com/project/cafelogproject/controller/PostController.java
@@ -35,11 +35,18 @@ public class PostController {
 
   private final PostService postService;
 
+  /**
+   * 새 게시글 작성 API
+   *
+   * @param request     AddPostRequestDTO - 게시글의 제목, 내용 등 포함.
+   * @param userDetails 인증된 사용자의 정보
+   */
   @PostMapping("/write")
   public ResponseEntity<PostDetailDTO> addPost(@RequestBody AddPostRequestDTO request,
       @AuthenticationPrincipal UserDetails userDetails) {
     log.info("Received request to add post from user: {}", userDetails.getUsername());
     try {
+
       PostDetailDTO response = postService.addPost(request, userDetails.getUsername());
       return ResponseEntity.status(HttpStatus.CREATED).body(response);
     } catch (CustomException e) {
@@ -48,6 +55,13 @@ public class PostController {
     }
   }
 
+  /**
+   * 게시글 수정 API
+   *
+   * @param id          수정할 게시글 ID.
+   * @param updateDTO   UpdatePostRequestDTO - 수정할 제목, 내용 등 포함.
+   * @param userDetails 인증된 사용자의 정보.
+   */
   @PutMapping("/{id}")
   public ResponseEntity<PostResponseDTO> updatePost(@PathVariable Long id,
       @RequestBody @Valid UpdatePostRequestDTO updateDTO,
@@ -56,21 +70,42 @@ public class PostController {
     return ResponseEntity.ok(updatedPost);
   }
 
+  /**
+   * 게시글 검색 API 특정 키워드(query)를 사용해 게시글을 검색할 때 호출.
+   *
+   * @param query    검색할 키워드 (Tag name, Cafe name)
+   * @param pageable 페이지네이션과 정렬 정보.
+   */
   @GetMapping("/search")
   public ResponseEntity<Page<PostDetailDTO>> searchPosts(@RequestParam String query,
       @PageableDefault(sort = "createdDate", direction = Sort.Direction.DESC) Pageable pageable) {
     return ResponseEntity.ok(postService.searchPosts(query, pageable));
   }
 
+  /**
+   * 모든 게시글 조회 API (페이징 적용) 모든 게시글을 조회할 때 호출.
+   *
+   * @param pageable
+   */
   @GetMapping
   public ResponseEntity<Page<PostDetailDTO>> getAllPosts(
       @PageableDefault(sort = "createdDate", direction = Sort.Direction.DESC) Pageable pageable) {
+    // PostService를 통해 모든 게시글 리스트를 반환합니다.
     return ResponseEntity.ok(postService.getAllPosts(pageable));
   }
 
+  /**
+   * 게시글 삭제 API 특정 게시글을 삭제할 때 호출.
+   *
+   * @param id          게시글 ID.
+   * @param userDetails 인증된 사용자의 정보.
+   * @return 성공시 204(NO CONTENT)를 반환.
+   * 인증되지 않은 사용자인 경우 401(UNAUTHORIZED)를 반환.
+   */
   @DeleteMapping("/{id}")
   public ResponseEntity<?> deletePost(@PathVariable Long id,
       @AuthenticationPrincipal UserDetails userDetails) {
+    // 사용자가 인증되지 않은 경우
     if (userDetails == null) {
       return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("User not authenticated");
     }
