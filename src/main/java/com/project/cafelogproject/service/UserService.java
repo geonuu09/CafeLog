b41838f8c6979f35a82ed04a838e46c60983a71b
LeeGeonu <rjsdndl909@gmail.com>
2024-08-20 23:36:45 +0900

    refactor: authentication services Bean 충돌 해결 - merge : UserService <- UserDetailService

--- src/main/java/com/project/cafelogproject/service/UserService.java
+++ src/main/java/com/project/cafelogproject/service/UserService.java
@@ -62,8 +62,8 @@ public class UserService implements UserDetailsService {
   }
 
   @Override
-  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-    return userRepository.findByEmail(username)
+  public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+    return userRepository.findByEmail(email)
         .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
   }
 }
