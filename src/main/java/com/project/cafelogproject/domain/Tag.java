f5d4f2fafc01a7a4e1e8b647c40d393a01628b99
LeeGeonu <rjsdndl909@gmail.com>
2024-08-20 16:06:09 +0900

    feat: add post creation and view features - 포스트 작성: 로그인한 사용자만이 작성할 수 있다. - 게시글 조회: 모든 사용자가 태그이름 및 카페이름으로 조회할 수 있다.

--- src/main/java/com/project/cafelogproject/domain/Tag.java
+++ src/main/java/com/project/cafelogproject/domain/Tag.java
@@ -1,7 +1,5 @@
 package com.project.cafelogproject.domain;
 
-import jakarta.persistence.Cacheable;
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -18,7 +16,7 @@ public class Tag {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
-  @Column(unique = true)
+
   private String name;
 
   public Tag(String name) {
